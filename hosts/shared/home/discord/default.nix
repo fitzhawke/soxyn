f623fbc1f888ce35611bfcfcb48404564ee9c661
FitzHawke <will@fitzhawk.com>
2025-06-24 13:35:54 -0400

    packages: add legcord and dorion discord clients

--- hosts/shared/home/discord/default.nix
+++ hosts/shared/home/discord/default.nix
@@ -1,6 +1,8 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
+    dorion
     dissent
+    legcord
 
     # may need to run once without OpenASAR and Vencord after update
     # openASAR appears to not download something discord needs
